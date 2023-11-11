276653429b9aa2d6600be49e490fc60c949a17e9
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-11-11 13:00:34 +0600

    get itinerarys done

--- src/module/itinerary/itinerary.router.ts
+++ src/module/itinerary/itinerary.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyJwt } from "../../middleware/verifyJwt";
 import { verifyItinerary } from "./itinerary.validation";
-import { createItinerary } from "./itinerary.controller";
+import { createItinerary, getItinerarys } from "./itinerary.controller";
 
 const router = Router();
 
 router.post('/create-itinerary', verifyJwt, verifyItinerary, createItinerary)
+router.get('/get-itinerarys', verifyJwt, getItinerarys)
 
 export default router
